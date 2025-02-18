6f8f588210c4b1cba49051d1ff2f33bd732d992b
990617Leo <1111838@chwjh.tp.edu.tw>
2025-02-18 15:55:35 +0800

    Add files via upload

---
+++ 專案1/Form1.cs
@@ -0,0 +1,25 @@
+namespace 專案1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "按我幹嘛";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
